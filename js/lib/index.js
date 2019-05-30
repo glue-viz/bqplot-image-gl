3910c8b42571234dc4a7f53318dd439e260f15c3
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-05-30 17:16:33 +0100

    Merge pull request #9 from astrofrog/es6
    Convert code to ES6

--- js/lib/index.js
+++ js/lib/index.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 // Export widget models and views, and the npm package version number.
 module.exports = require('./imagegl.js');
-module.exports['version'] = require('../package.json').version;
+module.exports.version = require('../package.json').version;
