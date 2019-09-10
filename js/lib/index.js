414d6f4068c07eba1c65c1b81ffaba96c2950009
Maarten A. Breddels <maartenbreddels@gmail.com>
2019-09-10 15:46:01 +0200

    refactor: move from commonjs to es6 module

--- js/lib/index.js
+++ js/lib/index.js
@@ -1,5 +1,5 @@
 /*jshint esversion: 6 */
 
 // Export widget models and views, and the npm package version number.
-module.exports = require('./imagegl.js');
-module.exports.version = require('../package.json').version;
+export * from "./imagegl.js";
+export const version = require('../package.json').version;
