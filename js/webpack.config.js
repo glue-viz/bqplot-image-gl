b0e9a882ff8638e2d254c74230f8268656d58167
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-03-19 16:13:16 +0100

    chore: give unique path to source mapped files

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var webpack = require('webpack');
 var version = require('./package.json').version;
 
 // Custom webpack rules are generally the same for all webpack bundles, hence
@@ -34,7 +35,8 @@ module.exports = [
         output: {
             filename: 'index.js',
             path: path.resolve(__dirname, '..', 'bqplot_image_gl', 'static'),
-            libraryTarget: 'amd'
+            libraryTarget: 'amd',
+            devtoolModuleFilenameTemplate: 'webpack://bqplot-image-gl/[namespace]/[resource-path]?[loaders]',
         },
         devtool: 'source-map',
         module: {
