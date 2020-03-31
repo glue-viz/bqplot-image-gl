c54b752c6223a58b3e2efb830daf8a20157a70bf
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-03-31 13:25:07 +0200

    fix: fixes embed distribution (used for voila)

--- js/lib/embed.js
+++ js/lib/embed.js
@@ -5,7 +5,4 @@
 // It differs from the notebook bundle in that it does not need to define a
 // dynamic baseURL for the static assets and may load some css that would
 // already be loaded by the notebook otherwise.
-
-// Export widget models and views, and the npm package version number.
-module.exports = require('./imagegl.js');
-module.exports.version = require('../package.json').version;
+export * from './index'
