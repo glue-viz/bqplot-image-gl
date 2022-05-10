c7de2fbd8a40d1550923d851150d6299eb9b3958
Maarten A. Breddels <maartenbreddels@gmail.com>
2022-05-10 11:59:25 +0200

    fix: allow over 8 orders of magnitude in zooming
    see scales-delta.glsl for details.
    
    The issues could be exposed using:
    import numpy as np
    from bqplot import Figure, LinearScale, Axis, ColorScale, Lines
    from bqplot_image_gl import LinesGL
    import ipywidgets as widgets
    
    N = int(1e5)
    x = np.arange(N) * 100 + 1e9
    y = np.cumsum(np.random.random(N)*2-1)
    y -= y.mean()
    
    scale_x = LinearScale(min=x.min(), max=x.min()+1, allow_padding=False)
    scale_y = LinearScale(allow_padding=False)
    scales = {'x': scale_x, 'y': scale_y}
    axis_x = Axis(scale=scale_x, label='x')
    axis_y = Axis(scale=scale_y, label='y', orientation='vertical')
    line_gl = LinesGL(x=x, y=y, scales=scales, colors=['orange'])
    marks = [line_gl]
    figure = Figure(scales=scales, axes=[axis_x, axis_y], marks=marks)
    display(figure)

--- js/shaders/scales-extra.glsl
+++ js/shaders/scales-extra.glsl
@@ -3,7 +3,7 @@
 #define SCALE_TYPE_LOG 2
 
 #ifdef USE_SCALE_X
-    uniform vec2 domain_x;
+    uniform vec3 domain_x;
     uniform vec2 range_x;
     #if SCALE_TYPE_x == SCALE_TYPE_LINEAR
         #define SCALE_X(x) scale_transform_linear(x, range_x, domain_x)
@@ -13,7 +13,7 @@
 #endif
 
 #ifdef USE_SCALE_Y
-    uniform vec2 domain_y;
+    uniform vec3 domain_y;
     uniform vec2 range_y;
     #if SCALE_TYPE_y == SCALE_TYPE_LINEAR
         #define SCALE_Y(x) scale_transform_linear(x, range_y, domain_y)
