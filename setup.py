ee9642dbc2e632c88af9e11f933dbc16fbb0d8b1
P. L. Lim <2090236+pllim@users.noreply.github.com>
2024-12-20 13:34:38 -0500

    Remove Python 3.6 part 1
    [ci skip]

--- setup.py
+++ setup.py
@@ -72,7 +72,6 @@ setup(
         'Intended Audience :: Science/Research',
         'Topic :: Multimedia :: Graphics',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
