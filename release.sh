441b562db31ab65ce81c9218463381b8b2765a5e
Maarten A. Breddels <maartenbreddels@gmail.com>
2024-12-24 15:23:35 +0100

    ci: push to main, not master

--- release.sh
+++ release.sh
@@ -3,4 +3,4 @@ set -e -o pipefail
 # usage: ./release.sh minor -n
 version=$(bump2version --dry-run --list $* | grep new_version | sed -r s,"^.*=",,)
 echo Version tag v$version
-bumpversion $* --verbose && git push upstream master v$version
+bumpversion $* --verbose && git push upstream main v$version
